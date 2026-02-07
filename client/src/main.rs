c8af5b625231f00b5101fb50d19788fb4da0ca7c
blackh-t <nache7383@uit.no>
2026-02-07 18:15:01 +0100

    refactor: Restructure Project Architecture

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,15 +3,16 @@ use std::{
     io::{self, Write},
 };
 
-use aes::cipher::{BlockDecryptMut, KeyIvInit, block_padding::Pkcs7, generic_array::GenericArray};
 use log::{debug, info, warn};
-use rand::{Rng, rngs::OsRng, thread_rng};
-use rsa::{Pkcs1v15Encrypt, RsaPublicKey, pkcs1::DecodeRsaPublicKey};
 use tokio::net::TcpStream;
 use utils::{
+    cipher::decrypt_data,
     logger::init_logger,
-    protocol::{Connection, recv_message, send_message},
-    types::{Aes256CbcDec, AppResult},
+    protocols::{
+        handshake::Handshake,
+        lvf::{recv_message, send_message},
+    },
+    types::AppResult,
 };
 
 #[tokio::main]
@@ -24,7 +25,7 @@ async fn main() -> AppResult<()> {
     let socket = TcpStream::connect(&server_addr).await?;
     info!("Connecting to {}...", server_addr);
 
-    let connection = handshake(socket).await?;
+    let connection = Handshake::client(socket).await?;
     info!("Secure connection established!");
 
     // User input
@@ -46,29 +47,6 @@ async fn main() -> AppResult<()> {
     Ok(())
 }
 
-async fn handshake(mut socket: TcpStream) -> AppResult<Connection> {
-    send_message(&mut socket, "HELLO".as_bytes()).await?;
-
-    info!("Waiting for Public key");
-    let rsa_pub = recv_message(&mut socket).await?;
-    let rsa_pub = String::from_utf8(rsa_pub)?;
-    let client_public_key = RsaPublicKey::from_pkcs1_pem(&rsa_pub)?;
-
-    info!("Generate AES-256");
-    let mut aes_key = [0u8; 32];
-    let mut rng = OsRng;
-    thread_rng().fill(&mut aes_key);
-
-    let encrypt_key = client_public_key.encrypt(&mut rng, Pkcs1v15Encrypt, &aes_key)?;
-    send_message(&mut socket, &encrypt_key).await?;
-    info!("Sent encrypted Session Key to Server.");
-
-    Ok(Connection {
-        socket,
-        key: aes_key.to_vec(),
-    })
-}
-
 async fn download_from(mut socket: TcpStream, key: Vec<u8>, filename: &str) -> AppResult<()> {
     send_message(&mut socket, filename.as_bytes()).await?;
     debug!("Share key: {:?}", key);
@@ -80,12 +58,7 @@ async fn download_from(mut socket: TcpStream, key: Vec<u8>, filename: &str) -> A
     }
 
     let (iv, ciphertext) = cipher_data.split_at_mut(16);
-    let key = GenericArray::from_slice(&key);
-    let iv = GenericArray::from_slice(iv);
-    let cipher = Aes256CbcDec::new(&key, &iv);
-
-    let data = cipher.decrypt_padded_mut::<Pkcs7>(ciphertext).unwrap();
-
+    let data = decrypt_data(&mut ciphertext.to_vec(), &key, iv)?;
     info!("got file {}", String::from_utf8(data.to_vec()).unwrap());
 
     Ok(())
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,17 @@
-use aes::cipher::{BlockEncryptMut, KeyIvInit, block_padding::Pkcs7, generic_array::GenericArray};
 use log::{debug, error, info, warn};
-use rand::{Rng, thread_rng};
-use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, pkcs1::EncodeRsaPublicKey, pkcs8::LineEnding};
 use std::{env, io};
 use tokio::{
     fs,
     net::{TcpListener, TcpStream},
 };
 use utils::{
+    cipher::encrypt_data,
     logger::init_logger,
-    protocol::{Connection, recv_message, send_message},
-    types::{Aes256CbcEnc, AppResult},
+    protocols::{
+        handshake::Handshake,
+        lvf::{recv_message, send_message},
+    },
+    types::AppResult,
 };
 
 #[tokio::main]
@@ -35,7 +36,7 @@ async fn main() -> io::Result<()> {
 }
 
 async fn connection_handler(socket: TcpStream) -> AppResult<()> {
-    let mut conn = handshake(socket).await?;
+    let mut conn = Handshake::server(socket).await?;
     info!("Secure connection established!");
 
     // File Transfer
@@ -58,57 +59,10 @@ async fn connection_handler(socket: TcpStream) -> AppResult<()> {
             }
         };
 
-        let file_len = file_content.len();
-        let block_size = 16;
-        file_content.resize(file_len + block_size, 0);
-
-        let mut iv = [0u8; 16];
-        thread_rng().fill(&mut iv);
-
-        let key = GenericArray::from_slice(&conn.key);
-        let iv = GenericArray::from_slice(&iv);
-        let cipher = Aes256CbcEnc::new(&key, &iv);
-
-        let encrypt_data = cipher
-            .encrypt_padded_mut::<Pkcs7>(&mut file_content, file_len)
-            .unwrap();
-
-        let mut final_packet = Vec::with_capacity(16 + encrypt_data.len());
-        final_packet.extend_from_slice(&iv);
-        final_packet.extend_from_slice(encrypt_data);
-
-        send_message(&mut conn.socket, &final_packet).await?;
+        let packet = encrypt_data(&mut file_content, &conn.key)?;
+        send_message(&mut conn.socket, &packet).await?;
         info!("File Transfered.");
     }
 
     Ok(())
-}
-
-async fn handshake(mut socket: TcpStream) -> AppResult<Connection> {
-    info!("Etablish handshake...");
-
-    let msg = recv_message(&mut socket).await?;
-    let msg = String::from_utf8(msg).unwrap_or_default();
-    if msg != "HELLO" {
-        return Err("Protocol mismatch: Expected HELLO".into());
-    }
-
-    info!("Generate RSA");
-    let mut rng = rand::rngs::OsRng;
-    let bits = 2048;
-    let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-    let pub_key = priv_key.to_public_key();
-
-    info!("Send pub_pem");
-    let pub_pem = pub_key.to_pkcs1_pem(LineEnding::LF)?;
-    send_message(&mut socket, pub_pem.as_bytes()).await?;
-
-    info!("Waiting for Encrypted AES Key...");
-    let encrypted_aes_key = recv_message(&mut socket).await?;
-    let aes_key = priv_key.decrypt(Pkcs1v15Encrypt, &encrypted_aes_key)?;
-
-    Ok(Connection {
-        socket,
-        key: aes_key,
-    })
 }
