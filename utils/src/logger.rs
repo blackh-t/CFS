852d5e192ce24601126d555135e36fcacc2e4acd
blackh-t <nache7383@uit.no>
2026-02-13 20:39:24 +0100

    logger: [lvl=Info] + no access control msg

--- utils/src/logger.rs
+++ utils/src/logger.rs
@@ -1,15 +1,17 @@
 use std::io::Write;
 
+use log::warn;
+
 pub fn init_logger() {
     // Initialize Logger here so it's ready immediately
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug"))
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info"))
         .write_style(env_logger::WriteStyle::Always)
         .format(|buf, record| {
             let style = buf.default_level_style(record.level());
 
             writeln!(
                 buf,
-                "[{}] [{style}{level:^9}] [{:^20}]{style:#} {}",
+                "[{}] [{style}{level:^9}{style:#}] [{:^20}] {}",
                 buf.timestamp(),
                 record.target().split("::").last().unwrap_or("SERVER"),
                 record.args(),
@@ -18,4 +20,6 @@ pub fn init_logger() {
             )
         })
         .init();
+
+    warn!("\x1b[31mTHIS PROTOCOL IS RUNNING WITHOUT ACCESS CONTROL\x1b[0m");
 }
